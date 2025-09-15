414f9808d6612bb0f468f6c3eaf060bfc9f56fe5
Ipoiop <132857734+Ipoiop@users.noreply.github.com>
2025-09-15 20:22:24 -0300

    todos los ejercicios

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant lambda" #-}
+{-# HLINT ignore "Redundant bracket" #-}
 module Expr
   ( Expr (..),
     recrExpr,
@@ -62,34 +65,38 @@ eval  = foldExpr (\x -> dameUno (x,x))
                  (\e1 e2 gen -> (fst (e1 gen) * fst (e2 gen),snd (e1 gen)))
                  (\e1 e2 gen -> (fst (e1 gen) / fst (e2 gen),snd (e1 gen)))
 
--- | toma n muestras de f y las pone en una lista
-genLista :: Int -> G Float -> ([Float], Gen)
-genLista n = error "COMPLETAR EJERCICIO 9, la fcion auxiliar genLista"
-
 -- | @armarHistograma m n f g@ arma un histograma con @m@ casilleros
 -- a partir del resultado de tomar @n@ muestras de @f@ usando el generador @g@.
 armarHistograma :: Int -> Int -> G Float -> G Histograma
-armarHistograma m n f = \gen -> (histograma m rango (fst lista), (snd lista))
-                        where lista = genLista n f
-                              rango = rango95 (fst lista)
+armarHistograma m n f = \gen -> (histograma m (rango95 (fst (genLista gen))) (fst (genLista gen)), (snd (genLista gen)))
+                          where genLista = muestra f n
 
 -- | @evalHistograma m n e g@ evalúa la expresión @e@ usando el generador @g@ @n@ veces
 -- devuelve un histograma con @m@ casilleros y rango calculado con @rango95@ para abarcar el 95% de confianza de los valores.
 -- @n@ debe ser mayor que 0.
 evalHistograma :: Int -> Int -> Expr -> G Histograma
-evalHistograma m n expr = error "COMPLETAR EJERCICIO 10"
+evalHistograma m n expr = armarHistograma m n (eval expr)
 
 -- Podemos armar histogramas que muestren las n evaluaciones en m casilleros.
 -- >>> evalHistograma 11 10 (Suma (Rango 1 5) (Rango 100 105)) (genNormalConSemilla 0)
--- (Histograma 102.005486 0.6733038 [1,0,0,0,1,3,1,2,0,0,1,1,0],<Gen>)
+-- (Histograma 100.03403 1.0675964 [0,0,3,0,0,1,1,2,0,2,0,1,0],<Gen>)
 
 -- >>> evalHistograma 11 10000 (Suma (Rango 1 5) (Rango 100 105)) (genNormalConSemilla 0)
--- (Histograma 102.273895 0.5878462 [239,288,522,810,1110,1389,1394,1295,1076,793,520,310,254],<Gen>)
+-- (Histograma 100.99388 0.81878525 [241,289,507,825,1080,1350,1426,1333,1117,754,539,287,252],<Gen>)
 
 -- | Mostrar las expresiones, pero evitando algunos paréntesis innecesarios.
 -- En particular queremos evitar paréntesis en sumas y productos anidados.
 mostrar :: Expr -> String
-mostrar = error "COMPLETAR EJERCICIO 11"
+mostrar = recrExpr show (\x y -> show x ++ "~" ++ show y)
+                        (\e1 e2 r1 r2 -> (maybeParen (parEnSuma e1) r1) ++ " + " ++ (maybeParen (parEnSuma e2) r2))
+                        (\e1 e2 r1 r2 -> (maybeParen (parEnResta e1) r1) ++ " - " ++ (maybeParen (parEnResta e2) r2))
+                        (\e1 e2 r1 r2 -> (maybeParen (parEnMult e1) r1) ++ " * " ++ (maybeParen (parEnMult e2) r2))
+                        (\e1 e2 r1 r2 -> (maybeParen (parEnDiv e1) r1) ++ " / " ++ (maybeParen (parEnDiv e2) r2))
+
+          where parEnSuma =  \e -> constructor e == CEResta || constructor e == CEMult || constructor e == CEDiv
+                parEnResta = \e -> constructor e == CEResta || constructor e == CESuma || constructor e == CEMult || constructor e == CEDiv
+                parEnMult =  \e -> constructor e == CEResta || constructor e == CESuma || constructor e == CEDiv
+                parEnDiv =   \e -> constructor e == CEResta || constructor e == CEMult || constructor e == CESuma
 
 data ConstructorExpr = CEConst | CERango | CESuma | CEResta | CEMult | CEDiv
   deriving (Show, Eq)
