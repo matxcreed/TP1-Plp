99888bdbc412ef639e039dd92f68bc931be8ae7a
MilagrosGV <123619506+MilagrosGV@users.noreply.github.com>
2025-10-14 23:45:08 -0300

    correccion ej 1

--- src/Util.hs
+++ src/Util.hs
@@ -2,10 +2,17 @@ module Util where
 
 -- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
 -- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
+
+--alinearDerecha :: Int -> String -> String
+--alinearDerecha n s
+--    | length s >= n = s
+--    | otherwise     = replicate (n-length s) ' ' ++ s
+
+
+-- correccion
 alinearDerecha :: Int -> String -> String
-alinearDerecha n s
-    | length s >= n = s
-    | otherwise     = replicate (n-length s) ' ' ++ s
+alinearDerecha n s = replicate (n-length(s)) ' ' ++ s
+
 
 
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
@@ -14,6 +21,8 @@ alinearDerecha n s
 -- El primer elemento de la lista es el índice 0.
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
 actualizarElem n f  =  zipWith (\i x -> if i == n then f x else x) [0..]
+
+
 
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
